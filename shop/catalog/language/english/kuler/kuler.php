267ee8785fdf4ecbcc18560628484bd6bd0d6a5e
Aniket Prabhu <Aniket Prabhu>
2016-06-14 00:00:12 +0530

    Logo and Items

--- shop/catalog/language/english/kuler/kuler.php
+++ shop/catalog/language/english/kuler/kuler.php
@@ -1,4 +1,4 @@
 <?php
 $_['text_product_details'] = 'Product Details';
-$_['text_carousel_title'] = 'Shop by brands';
+$_['text_carousel_title'] = 'Category';
 $_['text_related_products'] = 'YOU MAY ALSO LIKE';
