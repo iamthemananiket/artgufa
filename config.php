e955bebd898473113c75a22b269cfa388abc16f4
Aniket Prabhu <iamthemananiket@gmail.com>
2016-06-15 01:02:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,8 @@
 <?php
 // HTTP
 define('HTTP_SERVER', 'http://artgufa.com/store/');
-
 // HTTPS
 define('HTTPS_SERVER', 'http://artgufa.com/store/');
-
 // DIR
 define('DIR_APPLICATION', './catalog/');
 define('DIR_SYSTEM', './system/');
@@ -16,14 +14,13 @@ define('DIR_IMAGE', './image/');
 define('DIR_CACHE', './system/cache/');
 define('DIR_DOWNLOAD', './download/');
 define('DIR_LOGS', './system/logs/');
-
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', 'ap-cdbr-azure-east-c.cloudapp.net');
 //define('DB_USERNAME', 'rocket_kartpro');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'b0b15779afae41');
 //define('DB_PASSWORD', '006006');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '5c229032');
 define('DB_DATABASE', 'rocket_kartpro');
 define('DB_PREFIX', 'oc_');
 ?>
