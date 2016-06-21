2aa4ff0324849ff19fee0c96e188027ca5bef98f
Aniket Prabhu <Aniket Prabhu>
2016-06-21 20:49:45 +0530

    Update credentilas to localhost for branch

--- config.php
+++ config.php
@@ -5,6 +5,9 @@ define('HTTP_SERVER', './');
 // HTTPS
 //define('HTTPS_SERVER', 'http://artgufa.com/store/');
 define('HTTPS_SERVER', './');
+define('HTTP_SERVER', 'http://artgufa.com/store/');
+// HTTPS
+define('HTTPS_SERVER', 'http://artgufa.com/store/');
 // DIR
 define('DIR_APPLICATION', './catalog/');
 define('DIR_SYSTEM', './system/');
@@ -16,14 +19,15 @@ define('DIR_IMAGE', './image/');
 define('DIR_CACHE', './system/cache/');
 define('DIR_DOWNLOAD', './download/');
 define('DIR_LOGS', './system/logs/');
-
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-//define('DB_USERNAME', 'rocket_kartpro');
+//define('DB_HOSTNAME', 'ap-cdbr-azure-east-c.cloudapp.net'); For azure
+
 define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', '006006');
+//define('DB_USERNAME', 'b0b15779afae41'); Fot azure
 define('DB_PASSWORD', '');
+//define('DB_PASSWORD', '5c229032');
 define('DB_DATABASE', 'rocket_kartpro');
 define('DB_PREFIX', 'oc_');
 ?>
