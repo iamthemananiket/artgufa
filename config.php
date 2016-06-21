f9d7ae01e90f97fb82a9fce94fc32c04b4345bba
Aniket Prabhu <iamthemananiket@gmail.com>
2016-06-21 21:01:23 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('HTTP_SERVER', './');
 // HTTPS
 //define('HTTPS_SERVER', 'http://artgufa.com/store/');
 define('HTTPS_SERVER', './');
-define('HTTP_SERVER', 'http://artgufa.com/store/');
+//define('HTTP_SERVER', 'http://artgufa.com/store/');
 // HTTPS
 
 // DIR
@@ -22,14 +22,14 @@ define('DIR_LOGS', './system/logs/');
 // DB
 define('DB_DRIVER', 'mysqli');
 
-define('DB_HOSTNAME', 'localhost');
+//define('DB_HOSTNAME', 'localhost');
 
-//define('DB_HOSTNAME', 'ap-cdbr-azure-east-c.cloudapp.net'); For azure
+define('DB_HOSTNAME', 'ap-cdbr-azure-east-c.cloudapp.net'); For azure
 
-define('DB_USERNAME', 'root');
-//define('DB_USERNAME', 'b0b15779afae41'); Fot azure
-define('DB_PASSWORD', '');
-//define('DB_PASSWORD', '5c229032');
+//define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'b0b15779afae41'); Fot azure
+//define('DB_PASSWORD', '');
+define('DB_PASSWORD', '5c229032');
 define('DB_DATABASE', 'rocket_kartpro');
 define('DB_PREFIX', 'oc_');
 ?>
