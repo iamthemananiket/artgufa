81a828f155cf257e6b00443eb66fff1296ba9b42
Aniket Prabhu <iamthemananiket@gmail.com>
2016-06-21 21:12:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -24,10 +24,10 @@ define('DB_DRIVER', 'mysqli');
 
 //define('DB_HOSTNAME', 'localhost');
 
-define('DB_HOSTNAME', 'ap-cdbr-azure-east-c.cloudapp.net'); For azure
+define('DB_HOSTNAME', 'ap-cdbr-azure-east-c.cloudapp.net');
 
 //define('DB_USERNAME', 'root');
-define('DB_USERNAME', 'b0b15779afae41'); Fot azure
+define('DB_USERNAME', 'b0b15779afae41');
 //define('DB_PASSWORD', '');
 define('DB_PASSWORD', '5c229032');
 define('DB_DATABASE', 'rocket_kartpro');
