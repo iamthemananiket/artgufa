f5f7f2c03681e710ef108deb8fae67fea7599733
iamthemananiket <iamthemananiket@gmail.com>
2016-07-28 17:00:57 +0530

    Merge conflict solved

--- catalog/language/english/common/header.php
+++ catalog/language/english/common/header.php
@@ -4,8 +4,8 @@ $_['text_home']           = 'Home';
 $_['text_wishlist']       = 'Wish List (%s)';
 $_['text_shopping_cart']  = 'Shopping Cart';
 $_['text_search']         = 'Search';
-$_['text_welcome']        = 'Welcome visitor you can <a href="%s" style="color: white;">login</a> or <a href="%s" style="color: white;">create an account</a>.';
-$_['text_logged']         = 'You are logged in as <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
+$_['text_welcome']        = '<li><a href="%s" style="color: white;">Login</a></li><li><a id="create-account" href="%s" style="color: white;">Create an Account</a></li>';
+$_['text_logged']         = '<a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
 $_['text_account']        = 'My Account';
 $_['text_checkout']       = 'Checkout';
 ?>
